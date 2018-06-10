1d57b4a9d66d820699e8a38fb985d938fc99c0b8
Thomas Boutell <tom@punkave.com>
2018-06-10 18:36:26 -0400

    first working examples in this project: one, two, and three-column nested widgets, plus displaying thumbnail images for the tabs of the site

---
+++ app.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+var apos = require('apostrophe')({
+  shortName: 'apostrophe-samples',
+
+  // See lib/modules for basic project-level configuration of our modules
+  // responsible for serving static assets, managing page templates and
+  // configuring user acounts.
+
+  modules: {
+
+    // Apostrophe module configuration
+
+    // Note: most configuration occurs in the respective
+    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+    
+    // However any modules that are not present by default in Apostrophe must at
+    // least have a minimal configuration here: `moduleName: {}`
+
+    // If a template is not found somewhere else, serve it from the top-level
+    // `views/` folder of the project
+
+    'apostrophe-templates': { 
+      viewsFolderFallback: path.join(__dirname, 'views') 
+    },
+   
+    'one-column-widgets': {},
+    'two-column-widgets': {},
+    'three-column-widgets': {}
+
+  }
+});
